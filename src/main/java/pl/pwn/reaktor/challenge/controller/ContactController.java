53d5087334acb393946c1654509fd6651c20e963
Pablo <pawel.gaudziak@gmail.com>
2019-01-19 14:45:02 +0100

    mail

---
+++ src/main/java/pl/pwn/reaktor/challenge/controller/ContactController.java
@@ -0,0 +1,43 @@
+package pl.pwn.reaktor.challenge.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.pwn.reaktor.challenge.model.Contact;
+import pl.pwn.reaktor.challenge.service.EmailService;
+
+import javax.validation.Valid;
+
+@Controller
+public class ContactController {
+    private EmailService emailService;
+
+    @Autowired
+    public ContactController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+
+        Contact contact = new Contact();
+        model.addAttribute("contact", contact);
+        return "contact";
+    }
+
+    @PostMapping("/sentMessage")
+    public String sent(@Valid @ModelAttribute Contact contact, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "contact";
+        }
+
+        emailService.sendMessage(contact);
+        model.addAttribute("contact", new Contact());
+        return "contact";
+    }
+}
---
+++ src/main/java/pl/pwn/reaktor/challenge/service/EmailService.java
@@ -0,0 +1,34 @@
+package pl.pwn.reaktor.challenge.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import pl.pwn.reaktor.challenge.model.Contact;
+
+
+@Component
+public class EmailService {
+
+    private static final String TO = "pawel.gaudziak@gmail.com";
+    private static final String SUBJECT = "Kontakt z odpadów";
+
+    @Autowired
+    public JavaMailSender mailSender;
+
+    public void sendMessage(Contact contact) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(TO);
+        message.setSubject(SUBJECT);
+        message.setText(contact.getMessage());
+        message.setReplyTo(contact.getEmail());
+
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            e.printStackTrace();
+        }
+    }
+}
