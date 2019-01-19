16ef33546d2d0396323b245d6ec29573df5a2344
Pablo <pawel.gaudziak@gmail.com>
2019-01-19 13:07:04 +0100

    serwis 2

--- src/main/java/pl/pwn/reaktor/challenge/controller/KPOController.java
+++ src/main/java/pl/pwn/reaktor/challenge/controller/KPOController.java
@@ -1,7 +1,9 @@
 package pl.pwn.reaktor.challenge.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class KPOController {
 
     @GetMapping("/KPO")
--- src/main/java/pl/pwn/reaktor/challenge/service/LoginService.java
+++ src/main/java/pl/pwn/reaktor/challenge/service/LoginService.java
@@ -1,5 +1,8 @@
 package pl.pwn.reaktor.challenge.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
 }
