18c476e257dcdd66d68e48be2c9005691341beb9
Pablo <pawel.gaudziak@gmail.com>
2019-01-19 11:52:41 +0100

    controller 4

--- src/main/java/pl/pwn/reaktor/challenge/controller/LoginController.java
+++ src/main/java/pl/pwn/reaktor/challenge/controller/LoginController.java
@@ -1,4 +1,13 @@
 package pl.pwn.reaktor.challenge.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginController {
+
+    @PostMapping("/login")
+    public String logowanie(){
+        return "logowanie";
+    }
 }
